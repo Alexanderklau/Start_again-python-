7deda438745ae3cb376274f64a42b005a91ff001
Alexanderklau <429095816@qq.com>
2017-04-16 23:33:52 +0800

    start

---
+++ Loop_and_condition/simple_loop.py
@@ -0,0 +1,6 @@
+# -×- coding: utf-8 -*-
+__author__ = "Yemilice_lau"
+c = ['lau','yemilice','dsudua']
+d = [1,2,3,4]
+for name in range(len(c)):
+    print "liu",c[name]
---
+++ Try_except/Error_sys.py
@@ -0,0 +1,8 @@
+# -×- coding: utf-8 -*-
+__author__ = "Yemilice_lau"
+try:
+    float('123')
+except:
+    import sys
+    exc = sys.exc_info()
+    print exc
