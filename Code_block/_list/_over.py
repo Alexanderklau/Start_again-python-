9b562fc5d0753f69cc400de816621c6a1d3d83f3
Alexanderklau <429095816@qq.com>
2017-04-19 23:26:11 +0800

    python

---
+++ Code_block/_list/_over.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+__author__ = "Yemilice_lau"
+raw_input("\n\nPress the enter key to exit.")
---
+++ Code_block/_list/movie_example.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+__author__ = "Yemilice_lau"
+movies = ["The Holy Grail","Hello","TOO"]
+movies.insert(1,1995)
+movies.append(1983)
+movies.insert(3,1979)
+print movies
